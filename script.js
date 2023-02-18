bab5e12db17fcdcd6ed0c1cb33d23a43438a7864
MaxLyFe <maximilianolyfbuzios@gmail.com>
2023-02-18 18:11:19 -0300

    commit index, script and style completed

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-/* Obtener el elemento con el id de abierto. */
-const open = document.getElementById('open')
+/* Asignando la variable open al elemento con el id de open. */
+const open = document.getElementById('open');
 
-/* Obtener el elemento con el id de cierre. */
-const close = document.getElementById('close')
+/* Asignando la variable close al elemento con el id de close. */
+const close = document.getElementById('close');
 
-/* Obtener el elemento con la clase de contenedor. */
-const container = document.getElementById('.container')
+/* Seleccionando el elemento con la clase de contenedor. */
+const container = document.querySelector('.container');
 
 /* Agregar un detector de eventos al elemento abierto. Cuando se hace clic en el elemento abierto, la
-clase show-nav se agrega al elemento contenedor. */
-open.addEventListener('click', () => container.classList.add('show-nav'))
+clase de show-nav se agrega al elemento contenedor. */
+open.addEventListener('click', () => container.classList.add('show-nav'));
 
-/* Eliminando la clase show-nav del elemento contenedor. */
-close.addEventListener('click', () => container.classList.remove('show-nav'))
+/* Eliminando la clase de show-nav del contenedor. */
+close.addEventListener('click', () => container.classList.remove('show-nav'));
